c846270163de9231b78309f0887b03c32c9c2226
naleNST <148430534+naleNST@users.noreply.github.com>
2023-11-03 11:31:34 +0700

    Update config.php

--- auth/config.php
+++ auth/config.php
@@ -12,7 +12,7 @@ $password = "cQmEsVp2rJ";
 $database = "sql12658879";
 $port = "3306";
 
-$conn = new mysqli($servername, $username, $password, $database);
+$conn = new mysqli($servername, $username, $password, $database, $port);
 
 if ($conn->connect_error) {
     die("Koneksi database gagal: " . $conn->connect_error);
@@ -30,11 +30,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // die();
     if ($result->num_rows > 0) {
         // Jika login berhasil, Anda bisa mengarahkan pengguna ke halaman lain
-        header("Location: ../dashboard.php");
+        header("Location: ./dashboard.php");
         exit();
     } else {
          echo "<script>alert('Username / Password salah. Coba lagi.');</script>";
-         echo "<script>window.location = '../index.html';</script>"; 
+         echo "<script>window.location = './index.html';</script>"; 
     }
 }
 $conn->close();
